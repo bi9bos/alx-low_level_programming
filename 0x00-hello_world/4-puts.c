bdcd80b73f4b473d93706637a6105694c2311543
Faruq Tiamiyu <benignboss@gmail.com>
2022-07-17 18:27:52 -0700

    Intro to C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle\n");
 return (0);
 }
