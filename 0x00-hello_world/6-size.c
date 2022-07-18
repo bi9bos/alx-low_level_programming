41a73c2a1d4a546fce50ec6606c371d32f163cf6
Faruq Tiamiyu <benignboss@gmail.com>
2022-07-17 18:09:42 -0700

    Introduction to c programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */                                                               int main (void)                                                   {                                                                         char c;                                                           int i;
+        long int longi;
+        long long int lx2_i;
+        float f;
+        printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s) \n", (unsigned long) sizeof(i));
+	printf("Size of a long int: %lu bytes(s) \n", (unsigned long) sizeof(longi));
+	printf("Size of a long long int: %lu bytes(s) \n", (unsigned long) sizeof(lx2_i));
+	printf("Size of a float: %lu byte(s) \n", (unsigned long) sizeof(f));
+	return (0);
+}v
