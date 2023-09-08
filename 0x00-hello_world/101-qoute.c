e9a2da2b2357b4652a59d21056b98289abe7e1f9
Tiamiyu Faruq A <benignboss@gmail.com>
2023-09-07 18:35:03 -0700

    Latest update

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,5 +1,9 @@
 #include <unistd.h>
+#include <string.h>
+
 int main(void)
 {
-	write(2"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	write(2, message, strlen(message));
+	return (1);	
 }
