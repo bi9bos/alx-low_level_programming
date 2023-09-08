68590b541f15b9803505347f32685a5de150ec11
Tiamiyu Faruq A <benignboss@gmail.com>
2023-09-07 17:25:32 -0700

    Low level C programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,15 @@
 /**
  * main - Entry point
  * Return: Always 0 (Success)
- */                                                               
-int main(void)                                                   
-{                                                                         
-	char c;                                                           
+*/
+int main(void)
+{
+	char c;
 	int i;
-        long int longi;
- 	long long int lx2_i;
-        float f;
-
-        printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(c));
+	long int longi;
+	long long int lx2_i;
+	float f;
+	printf("Size of a char: %lu byte(s) \n", (unsigned long) sizeof(c));
 	printf("Size of an int: %lu byte(s) \n", (unsigned long) sizeof(i));
 	printf("Size of a long int: %lu bytes(s) \n", (unsigned long) sizeof(longi));
 	printf("Size of a long long int: %lu bytes(s) \n", (unsigned long) sizeof(lx2_i));
