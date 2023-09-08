f9dd26c718a3ffb74249986c015d37f367717d0d
Tiamiyu Faruq A <benignboss@gmail.com>
2023-09-07 19:02:32 -0700

    g

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,16 @@
 #include <unistd.h>
 #include <string.h>
-
+/*
+ * This return the output as error
+ * Return 1 as success
+ */
 int main(void)
 {
-	const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-	write(2, message, strlen(message));
-	return (1);	
+/*
+ *Main -> It creates a message  variable and then save it as  error
+ */
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, strlen(msg));
+	return (1);
 }
