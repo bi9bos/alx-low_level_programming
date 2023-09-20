ebff83bf292447eaaa85af7f8ef04ea8e63bddbb
Tiamiyu Faruq A <benignboss@gmail.com>
2023-09-19 19:15:45 -0700

    New commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
-	
+
 	while (s[len] != '\0')
 		len++;
 
@@ -38,14 +38,14 @@ int _atoi(char *s)
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
-			
+
 			f = 0;
 		}
 
 		i++;
 
 	}
-	
+
 	if (f == 0)
 		return (0);
 
